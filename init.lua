6529e6362889d2f3d46762a19846135bc764646e
R1das <217116004+R1das67@users.noreply.github.com>
2026-04-02 14:53:45 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 local successMain, errMain = pcall(function()
-    loadstring(game:HttpGet(""))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Main.lua")) ()
+    end)
+
+local successGui, errGui = pcall(function()
+    loadstring(game:HttpGet("

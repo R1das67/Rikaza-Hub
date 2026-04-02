14291fc38fbc065341d0c2f1ed973668b0f92d45
R1das <217116004+R1das67@users.noreply.github.com>
2026-04-02 15:27:28 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
-local successMain, errMain = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Main.lua")) ()
-    end)
+local function load(url)
+    local success, result = pcall(game.HttpGet, game, url)
+    if success then
+        local func, err = loadstring(result)
+        if func then
+            func()
+        else
+            warn("Fehler im Code der Datei: " .. tostring(err))
+        end
+    else
+        warn("Link konnte nicht geladen werden: " .. tostring(result))
+    end
+end
 
-local successGui, errGui = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Gui.lua") ()
-        end)
+load("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Main.lua")
+load("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Gui.lua")

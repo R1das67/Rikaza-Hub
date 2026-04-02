8fc7e9bd514a5a643ff529d59e31ead31546b84f
R1das <217116004+R1das67@users.noreply.github.com>
2026-04-02 16:04:05 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,22 +1,23 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
-local ReplicatedStorage =
-game:GetService("ReplicatedStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LocalPlayer = Players.LocalPlayer
 
-(Blade Bal Remotes)
-local Remotes =
-ReplicatedStorage:WaitForChild("Remotes",5)
-local ParryRemote = Remotes and
-(Remotes:FindFirstChild("ParryButtonPress") or
-Remotes:FindFirstChild("ParryAttempt"))
+local Remotes = ReplicatedStorage:WaitForChild("Remotes", 5)
+local ParryRemote = nil
+
+if Remotes then
+    ParryRemote = Remotes:FindFirstChild("ParryButtonPress") 
+               or Remotes:FindFirstChild("ParryAttempt") 
+               or Remotes:FindFirstChild("RemoteEvent")
+end
 
 _G.AutoShoot = false
 
 local function getCurrentBall()
-    local ballFolder = workspace:FindFirstChild("Ball")
+    local ballFolder = workspace:FindFirstChild("Balls")
     if ballFolder then
-        for _, ball in pars(ballFolder:GetChildren()) do
+        for _, ball in pairs(ballFolder:GetChildren()) do
             if ball:GetAttribute("realBall") == true then
                 return ball
             end
@@ -27,34 +28,28 @@ end
 
 RunService.PostSimulation:Connect(function()
     if not _G.AutoShoot then return end
-
+    
     local character = LocalPlayer.Character
-    local rootPart = character and
-    character:FindFirstChild("HumanoidRootPart")
+    local rootPart = character and character:FindFirstChild("HumanoidRootPart")
     local ball = getCurrentBall()
-
+    
     if ball and rootPart then
         local ballPos = ball.Position
         local playerPos = rootPart.Position
-
         local distance = (ballPos - playerPos).Magnitude
-
-        local velocity 0 ball.AssemblyLinearVelocity
+        local velocity = ball.AssemblyLinearVelocity
+        
         local ballDirection = (playerPos - ballPos).Unit
-        local isComingTowardsMe =
-    velocity.Unit:Dot(ballDirection)
-
-    (DotProduct > 0)
-        if isComingTowardsMe > 0 then
+        local isComingTowardsMe = velocity.Unit:Dot(ballDirection)
+        
+        if isComingTowardsMe > 0.1 then
             local speed = velocity.Magnitude
-
-            local dynamicRange = 10 + (speed * 0.15)
-
+            local dynamicRange = 12 + (speed * 0.2)
+            
             if distance <= dynamicRange then
-
-               if ParryRemote then
-                  ParryRemote:FireServer()
-               end
+                if ParryRemote then
+                    ParryRemote:FireServer()
+                end
             end
         end
     end

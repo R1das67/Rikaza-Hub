d78c8b457ec6a365f3a6714c118fb8f47b4b066c
R1das <217116004+R1das67@users.noreply.github.com>
2026-04-03 19:47:35 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,16 @@
-_G.AutoShoot = false
+_G.BetterRivalsSettings = {
+    AutoAim = false,
+    AutoShoot = false,
+    Fly = false,
+    ReactionTime = 1,
+    FlyHeight = 5,
+    FlySpeed = 1,
+    Active = true
+}
 
-local function load(url)
-    local success, result = pcall(game.HttpGet, game, url)
-    if success then
-        local func, err = loadstring(result)
-        if func then
-            func()
-        else
-            warn("Error: " .. tostring(err))
-        end
-    else
-        warn("Link Error: " .. tostring(result))
-    end
-end
+local gui_raw = "https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Gui.lua"
+local main_raw = "https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Main.lua"
 
-load("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Gui.lua")
-load("https://raw.githubusercontent.com/R1das67/Rikaza-Hub/refs/heads/main/Main.lua")
+loadstring(game:HttpGet(gui_raw))()
+task.wait(0.5)
+loadstring(game:HttpGet(main_raw))()
